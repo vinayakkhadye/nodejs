87338b789188e7492efbef84fe8b303d28fefcae
vinayak khadye <vinayak249@gmail.com>
2017-07-19 17:24:07 +0530

    comit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ http.createServer(function (request, response) {
    
    // Send the response body as "Hello World"
    response.end('Hello World\n');
-}).listen(80);
+}).listen(3306);
 
 // Console will print the message
-console.log('Server running at http://52.91.147.33:80/');
+console.log('Server running at http://52.91.147.33:3306/');
