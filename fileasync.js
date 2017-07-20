734e2a6c88b2b73017330fde1e7624d5a982145f
root <root@ip-172-31-162-110.ec2.internal>
2017-07-20 11:51:06 +0000

    file asyn changes

--- fileasync.js
+++ fileasync.js
@@ -1,8 +1,8 @@
 var fs	= require('fs');
 
 fs.readFile('input.txt',function(err,data){
-	if(err){return console.log(err);}
-	console.log(return data.toString());
+	if(err){return console.error(err);}
+	console.log(data.toString());
 });
 
 console.log("Program Ended");
