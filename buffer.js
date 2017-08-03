30529cf006c9947bef82b7ee22c5974c1eebc8a3
root <root@ip-172-31-162-110.ec2.internal>
2017-08-03 10:55:00 +0000

    buffer cfhanges

--- buffer.js
+++ buffer.js
@@ -1,3 +1,13 @@
-var buf	= new Buffer(["HI Vinayak",4,65,5,"ok"]);
-//buf.write(["HI Vinayak",4,65,5,"ok"]);
-console.log(buf.toString());
+var buf1 = new Buffer(100);
+var buf2 = new Buffer([10,0,0]);
+var buf3 = new Buffer("Simple Easy Learning","utf-8");
+
+buf2.write("vinayakkhadye");
+for (var i=0; i<97; i++){
+	buf1[i]	= i+34;
+}
+var json = buf1.toJSON(buf1);
+console.log(buf1);
+console.log("buf2 "+ buf2.toString());
+console.log("buf3 "+ buf3.toString());
+
