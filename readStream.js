d85b67440e4de9c998437301d967eca93a3f1d52
vinayak khadye <vinayak249@gmail.com>
2017-08-03 17:51:08 +0530

    Read stream

---
+++ readStream.js
@@ -0,0 +1,22 @@
+var fs = require("fs");
+var data = '';
+
+var readStream = fs.createReadStream('input.txt');
+
+readStream.setEncoding('utf-8');
+
+readStream.on('data',function(chunk){
+	data += chunk;
+});
+
+readStream.on('end',function(){
+	console.log(data);
+});
+
+readStream.on('error',function(err){
+	console.log(err.stack);
+});
+
+readStream.on('finish',function(){
+	console.log("all finished");
+});
